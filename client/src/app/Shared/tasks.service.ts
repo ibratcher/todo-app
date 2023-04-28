6e8e147ff0e86cf4914e4a26822dd5fab8efcd0d
ibratcher <ibratcher@cynerge.com>
2023-04-28 12:02:14 -0400

    Implementing DB stuff.

--- client/src/app/Shared/tasks.service.ts
+++ client/src/app/Shared/tasks.service.ts
@@ -16,29 +16,33 @@ export class TasksService {
   rootURL = '/api/';
 
   private tasks: Task[] = [
-    new Task('Task 1', "This is task 1", false),
-    new Task('Task 2', 'This is task 2', false),
-    new Task('Task 3', 'This is task 3', false),
-    new Task('Task 4', 'This is task 4', false),
+    new Task('Task 1', 'This is the first task', false),
+    new Task('Task 2', 'This is the second task', false),
+    new Task('Task 3', 'This is the third task', false),
+    new Task('Task 4', 'This is the fourth task', false),
   ];
 
   constructor(private http: HttpClient) {
+    this.getTasksFromBackend()
+    console.log(this.tasks);
   }
 
-  //Grab all tasks from the backend
   getTasksFromBackend() {
     this.http.get<Task[]>(this.rootURL + 'task').subscribe((tasks: Task[]) => {
-      this.tasks = tasks;
+      console.log(tasks);
       this.tasksChanged.next(this.tasks.slice());
+      return tasks;
     });
   }
 
-  getTasks() {
+  postTaskToBackend(task: Task) {
     this.http.post<Task[]>(this.rootURL + 'task', this.tasks).subscribe((tasks: Task[]) => {
-        this.tasks = tasks;
-        this.tasksChanged.next(this.tasks.slice());
+        console.log(tasks);
       }
     );
+  }
+
+  getTasks() {
     return this.tasks.slice();
   }
 }
--- server/www.js
+++ server/www.js
@@ -1,16 +1,34 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
+
+// Postgres Config
+const {Pool} = require('pg');
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT
+});
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
 
 let task = [];
 const rootUrl = '/api';
 
 app.use(bodyParser.json());
 
-app.get(`${rootUrl}/task`, (req, res) => { res.json(task); });
+app.get(`${rootUrl}/task`, (req, res) => {
+  res.json(task);
+});
 
 app.post(`${rootUrl}/task`, (req, res) => {
-  const newTask = req.body.task;
+  const newTask = req.body;
   task = [];
   task.push(newTask);
   res.json(newTask);
