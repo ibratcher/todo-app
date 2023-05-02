309337be740caf3f20b7c28cdb9fbfdb91ed2324
ibratcher <isaac.j.bratcher@gmail.com>
2023-05-02 14:08:36 -0400

    Adding functionality to actually add this task to the homepage and upload to the database.

--- client/src/app/create-task/create-task.component.ts
+++ client/src/app/create-task/create-task.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import {Task, TasksService} from "../Shared/tasks.service";
 
 @Component({
   selector: 'app-create-task',
@@ -7,15 +8,26 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./create-task.component.css']
 })
 export class CreateTaskComponent {
-  addressForm = this.fb.group({
-    taskTitle: [null,Validators.required],
-    description: [null, Validators.required],
-    isComplete: ['false', Validators.required]
+  taskForm = this.fb.group({
+    taskTitle: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    isComplete: new FormControl('false', Validators.required)
   });
 
-  constructor(private fb: FormBuilder) {}
+
+  constructor(private fb: FormBuilder, private tasksService: TasksService) {}
 
   onSubmit(): void {
-    alert('Thanks!');
+    const title = <string>this.taskForm.value.taskTitle;
+    const description = <string>this.taskForm.value.description;
+    const isComplete = (this.taskForm.value.isComplete === 'true'); // convert string to boolean
+    const task: Task = new Task(title, description, isComplete);
+    this.tasksService.addTask(task);
+    this.taskForm.reset();
+
+    // Clears form errors after submission, which .reset() does not do
+    Object.keys(this.taskForm.controls).forEach(key => {
+      this.taskForm.get(key)!.setErrors(null) ;
+    });
   }
 }
