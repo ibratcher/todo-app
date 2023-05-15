57c524191cc85b1f4bc36d83e0de2430bec0a741
ibratcher <isaac.j.bratcher@gmail.com>
2023-05-15 10:26:41 -0400

    Implementing a built-in AuthGuard from Auth0 by Okta library.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {CreateTaskComponent} from "./create-task/create-task.component";
 import {CompletedComponent} from "./completed/completed.component";
+import {AuthGuard} from "@auth0/auth0-angular";
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'new', component: CreateTaskComponent},
-  {path: 'completed', component: CompletedComponent}
+  {path: 'new', component: CreateTaskComponent, canActivate: [AuthGuard]},
+  {path: 'completed', component: CompletedComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
