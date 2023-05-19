32110447062a9a63fa6d63961ec46dc77b639f27
ibratcher <isaac.j.bratcher@gmail.com>
2023-05-19 15:57:36 -0400

    Hopefully allows the api to run successfully.

--- client/src/app/Shared/tasks.service.ts
+++ client/src/app/Shared/tasks.service.ts
@@ -14,7 +14,7 @@ export class Task {
 
 export class TasksService implements OnDestroy{
   tasksChanged = new Subject<Task[]>();
-  rootURL = '/api/';
+  rootURL = 'ec2-3-15-149-3.us-east-2.compute.amazonaws.com:3080/api/';
 
   private tasks: Task[] = [];
 
